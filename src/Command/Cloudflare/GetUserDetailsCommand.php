1f5999e874ad16888f85342b48a70373ef3ef8a1
Jeroen de Bruin <jeroen@cupella.nl>
2019-07-10 14:50:14 +0200

    UserDetails command added.

--- src/Command/Cloudflare/GetUserDetailsCommand.php
+++ src/Command/Cloudflare/GetUserDetailsCommand.php
@@ -10,7 +10,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class UpdateIpAddressCommand extends Command
+class GetUserDetailsCommand extends Command
 {
     /** @var CloudflareApiService */
     private $apiService;
@@ -28,13 +28,20 @@ class UpdateIpAddressCommand extends Command
 
     protected function configure(): void
     {
-        $this->setName('jdd:cloudflare:updateipaddress')
-            ->setDescription('Update IP address for record in Cloudflare.');
+        $this->setName('jdd:cloudflare:userdetails')
+            ->setDescription('Get user details from Cloudflare.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Starttime: ' . $this->requestTime->format('Y-m-d H:i:s'));
+
+        $user = $this->apiService->getUser();
+
+        $output->writeln('User ID: ' . $user->getUserID());
+        $output->writeln('User Email: ' . $user->getUserEmail());
+        $output->writeln('User Details: ' . \json_encode($user->getUserDetails()));
+        $output->writeln('User Body: ' . \json_encode($user->getBody()));
 
         $output->writeln('Endtime: ' . $this->requestTime->format('Y-m-d H:i:s'));
     }
