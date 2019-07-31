a58a7f15fec9333d7804fa07496cc213e09d8ee3
Jeroen de Bruin <jeroen@cupella.nl>
2019-07-31 11:44:31 +0200

    Composer update

--- config/bundles.php
+++ config/bundles.php
@@ -3,6 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
